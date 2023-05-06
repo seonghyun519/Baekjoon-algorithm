e2cad21624db9a6807c34022455e6ba97a4e8cab
Sparklep <106800906+seonghyun519@users.noreply.github.com>
2023-05-06 23:36:20 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 124 ms, Memory: 14104 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int cnt = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= cnt; i++) {
+            for (int j = 0; j < i; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+        br.close();
+    }
+}
