090a2266afb8e181db0a7fe9d07d6a3770713d7a
Sparklep <106800906+seonghyun519@users.noreply.github.com>
2023-07-29 23:57:17 +0900

    [Silver V] Title: 분수찾기, Time: 160 ms, Memory: 16016 KB -BaekjoonHub

---
+++ 백준/Silver/1193. 분수찾기/분수찾기.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int X = Integer.parseInt(br.readLine());
+		int i = 0;
+		int val = 0;
+		while(val < X) {
+			i++;
+			val += i;
+		}
+		
+		if(i % 2 == 0) {
+			System.out.println((i - (val - X)) + "/" + (i + (((val - i) - X) + 1)));
+		}else {
+			System.out.println((i + (((val - i) - X) + 1)) + "/" + (i - (val - X)));
+		}
+	}
+
+}
