0627b7336645c9c33ae52460e6794a63a109cc94
Sparklep <106800906+seonghyun519@users.noreply.github.com>
2023-11-02 23:49:06 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 132 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        long n = Long.parseLong(br.readLine());
+
+        sb.append(n*(n-1)*(n-2)/6).append("\n");
+        sb.append(3).append("\n");
+
+        System.out.print(sb);
+        br.close();
+    }
+}
